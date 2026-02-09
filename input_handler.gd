b284d3189d25005ecec39c1bd5d6eab520dcef14
BrendanA1622 <89553639+aeriab@users.noreply.github.com>
2026-02-09 00:05:42 -0800

    Balls launch correctly

--- input_handler.gd
+++ input_handler.gd
@@ -12,7 +12,7 @@ var ball: RigidBody2D
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("spawn_piece"):
 		launch_point = DisplayServer.mouse_get_position()
-		print("launch point: " + str(launch_point))
+		#print("launch point: " + str(launch_point))
 		ball = ball_scene.instantiate()
 		ball.freeze = true
 		add_child(ball)
@@ -24,6 +24,16 @@ func _input(event: InputEvent) -> void:
 	
 	if event.is_action_released("spawn_piece"):
 		release_point = DisplayServer.mouse_get_position()
-		print("release point: " + str(release_point))
+		#print("release point: " + str(release_point))
+		
+		var y_diff: float = release_point.y - launch_point.y
+		var x_diff: float = release_point.x - launch_point.x
+		
+		print("y diff: " + str(y_diff))
+		print("x diff: " + str(x_diff))
+		
+		var launch_velocity: Vector2 = Vector2(-x_diff,-y_diff)
+		print("velo: " + str(launch_velocity))
 		
 		ball.freeze = false
+		ball.linear_velocity = launch_velocity
