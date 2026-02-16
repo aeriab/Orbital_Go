edee6090eed1916c17c51f49259811935784e468
Brendan Aeria <89553639+aeriab@users.noreply.github.com>
2026-02-15 22:16:31 -0800

    Proper stone point counting

--- game/zone_area_2d.gd
+++ game/zone_area_2d.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 @export var zone_collision_shape: CollisionShape2D
 
+@export var CaptureManager: Node
+
 func _ready() -> void:
 	# Connect to the Global signal we renamed in the last step
 	Global.zone_radius_changed.connect(_on_zone_radius_changed)
@@ -21,3 +23,17 @@ func _on_zone_radius_changed(new_radius: float) -> void:
 # to see if a stone has drifted too far.
 func is_point_inside(global_pos: Vector2) -> bool:
 	return global_pos.length() <= Global.zone_radius
+
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.get_parent() is Stone:
+		print("area...")
+		# call_deferred waits until the "Idle" time at the end of the frame
+		CaptureManager.call_deferred("UpdateTerritoryScore")
+
+func _on_area_exited(area: Area2D) -> void:
+	if area.get_parent() is Stone:
+		print("area exit...")
+		# call_deferred waits until the "Idle" time at the end of the frame
+		CaptureManager.call_deferred("UpdateTerritoryScore")
